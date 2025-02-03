95568462efe7891cd8bf15e70671497c4903a52f
watanabetatsumi <watanabe.tatsumi@icloud.com>
2025-02-03 10:43:04 +0900

    一旦push

--- TCPIP/arp.go
+++ TCPIP/arp.go
@@ -1,6 +1,11 @@
 package tcpip
 
-import "github.com/WIP2025winter/TCPIP/entity"
+import (
+	"log"
+	"syscall"
+
+	"github.com/WIP2025winter/TCPIP/entity"
+)
 
 func NewArpRequest(local LocalIpMacAddr, targetIP string) entity.ArpFrame {
 	arp := entity.ArpFrame{
@@ -28,5 +33,66 @@ func NewArpRequest(local LocalIpMacAddr, targetIP string) entity.ArpFrame {
 }
 
 func (*entity.ArpFrame) Send(dstMacAddr int, packet []byte) entity.ArpFrame {
+	// syscall.SockaddrLinkLayerという形（RAWソケット向け）を介して、カーネル空間のSocketディスクリプタに
+	// 書き込む。
+	addr := syscall.SockaddrLinklayer{
+		Protocol: syscall.ETH_P_ALL,    // すべてのプロトコル
+		Ifindex:  dstMacAddr,           // どのネットワークインターフェースを使うか指定。（引数のマックアドレス）
+		Hatype:   syscall.ARPHRD_ETHER, // イーサーネットフレーム
+	}
 
+	// "ソケット"を作成。->ソケットディスクリプタ（インターフェース）をオープン
+	// ソケットディスクリプタをカーネル空間から持ち上げて、通信を開始（読み込み/書き込み）。
+	// 通信の際のオプションは
+	// syscall.AF_PACKET: 通信のレベルを指定。
+	// syscall.SOCKRAW: デバイスドライバから受け取る。
+	// syscall.プロトコルを指定。->すべて
+	//  傍受したいのは、イーサーネットフレームのうちARP部分なのか、IPパケット部分なのか、ペイロード部分なのか等指定。
+	sendfd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(htons(syscall.ETH_P_ALL)))
+	if err != nil {
+		log.Fatal("create sendfd err : %v\n", err)
+	}
+	defer syscall.Close(sendfd)
+
+	// sendfdにpacketの内容を書き込んで、addr宛に送信(0はフラグ)
+	// 書き込み専用ソケット
+	err = syscall.Sendto(sendfd, packet, 0, &addr)
+	if err != nil {
+		log.Fatalf("Send to err : %v\n", err)
+	}
+
+	// レスポンスをListenするために待ち続ける -> forループ
+	for {
+		// ARPパケット自体は28バイト
+		// イーサーネットとARPは同じL2レイヤ->気にする必要はない。
+		recvBuf := make([]byte, 80)
+		// sendfdディスクリプタ（インターフェース）を介して、レスポンスを読み取り、recvBufに書き込む
+		_, _, err := syscall.Recvfrom(sendfd, recvBuf, 0)
+		if err != nil {
+			log.Fatalf("read err : %v\n", err)
+		}
+
+		if recvBuf[12] == 0x08 && recvBuf[13] == 0x06 {
+			//  ARPのopcodeがReply(0x0002)かチェック
+			if recvBuf[20] == 0x00 && recvBuf[21] == 0x02 {
+				// 14バイト目以降のARPパケットのペイロードを読み込む。
+				return parseArpPacket(recvBuf[14:])
+			}
+		}
+	}
+}
+
+// byteオーダーを構造体にマッピングする関数
+func parseArpPacket(packet []byte) entity.ArpFrame {
+	return entity.ArpFrame{
+		HardwareType:  []byte{packet[0], packet[1]},
+		ProtocolType:  []byte{packet[2], packet[3]},
+		HardewareSize: []byte{packet[4]},
+		ProtocolSize:  []byte{packet[5]},
+		Opcode:        []byte{packet[6]},
+		SenderMacAddr: packet[8:14],
+		SenderIpAddr:  packet[14:18],
+		TargetMacAddr: packet[18:24],
+		TargetIpAddr:  packet[24:28],
+	}
 }
--- TCPIP/utils.go
+++ TCPIP/utils.go
@@ -17,9 +17,19 @@ func Ip2Byte(ip string) []byte {
 	var ipbyte []byte
 
 	for _, v := range strings.Split(ip, ".") {
+		// 8bitのunsinged int（0～255）に変換
 		i, _ := strconv.ParseUint(v, 10, 8)
+		// 明示的に0,1で表現
 		ipbyte = append(ipbyte, byte(i))
 	}
 
 	return ipbyte
+}
+
+// CPU標準のリトルエディアンから、ビックエディアン（ネットワークバイトオーダー）に変換
+// 0x1234 -> 0x3412
+func htons(i uint16) uint16 {
+	// ２バイト長の数字の下位半分を左シフトして取り出す。-> 0xff00（マスク）を掛けて(AND演算)、低位バイトをゼロクリア
+	// 上位半分を右シフトして取り出した後、OR演算
+	return (i<<8)&0xff00 | i>>8
 }
